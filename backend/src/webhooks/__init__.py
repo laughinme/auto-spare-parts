7b8d2e06bd3385557db1a6842a9c1fb4ddd13c3b
Mark <wixw1cky7@gmail.com>
2025-08-22 01:38:08 +0700

    Fix product media uploading

--- backend/src/webhooks/__init__.py
+++ backend/src/webhooks/__init__.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter
 def get_webhooks() -> APIRouter:
     from .stripe import get_stripe_webhooks
     
-    webhooks = APIRouter(prefix='/webhooks', tags=['Webhooks'])
+    webhooks = APIRouter(prefix='/webhooks', tags=['Webhooks'], include_in_schema=False)
     
     webhooks.include_router(get_stripe_webhooks())
     
